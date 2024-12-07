308c42bace1b79d8d1d6e454300858b17243eb2e
AmirMahdiAsghari <amirmahdiasghari2022@gmail.com>
2024-12-07 15:22:12 +0330

    over add list and style

--- ganadi-reyazi/src/componnets/Navbar/index.jsx
+++ ganadi-reyazi/src/componnets/Navbar/index.jsx
@@ -1,17 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from '../../assets/img/logo/Logo.png'
+import Logo from "../../assets/img/logo/Logo.png";
 function Navbar() {
   return (
     <>
       <header>
+        {/* --------------Button start-------- */}
+        <div id="btn-start">
+          <Link>بزن بریم</Link>
+        </div>
+        {/* -----------list ----------- */}
+        <div id="list">
+          {" "}
+          <ul>
+          <Link>
+              <li>درباره ما</li>
+            </Link>
+         
+            <Link>
+              <li>تمرین ها</li>
+            </Link>
+          
+            <Link>
+              <li>دوره ها</li>
+            </Link>
+            <Link>
+              <li>خانه</li>
+            </Link>
+            
+          </ul>
+        </div>
+        {/* ----------Logo */}
         <div id="Logo">
-          <Link to='/'>
+          <Link to="/">
             <img src={Logo} alt="" />
           </Link>
         </div>
-        <div id="list"></div>
-        <div id="btn-start"></div>
       </header>
     </>
   );
